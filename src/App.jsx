978ed17b7a21235e783889e5e3212550277f0067
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-17 16:11:24 -0300

    Estilizando y rellenando el componente task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { createGlobalStyle } from 'styled-components'
+import {generate as id} from 'shortid'
 import allColors from './styles/colors'
 import FormTask from './components/FormTask'
 import Task from './components/Task'
+
 
 
 const GlobalSyle = createGlobalStyle`
@@ -52,7 +54,11 @@ class App extends Component {
                 <div>
                     {
                         tasks.map(task => (
-                            <Task></Task>
+                            <Task
+                                key={id()} 
+                                title={task.title}
+                                color={task.color}
+                            />
                         ))
                     }
                 </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,6 +5,13 @@ import styled from 'styled-components'
 const TaskContainer = styled.div`
     display: grid;
     grid-template-columns: 30px 1fr 50px;
+    justify-items: start;
+    align-items: center;
+    padding: 0 1rem;
+    width: 100%;
+    max-width: 250px;
+    margin: 1rem auto;
+    background-color: ${({color}) => color};
 `
 
 const TaskButton = styled.button`
@@ -12,13 +19,14 @@ const TaskButton = styled.button`
 `
 
 const TaskText = styled.p`
-
+    font-size: .8rem;
+    color: #fff;
 `
 
-const Task = () => (
-    <TaskContainer>
+const Task = ({ color, title }) => (
+    <TaskContainer color={color}>
         <input type="checkbox" />
-        <TaskText></TaskText>
+        <TaskText>{title}</TaskText>
         <TaskButton>Delete</TaskButton>
     </TaskContainer>
 )
